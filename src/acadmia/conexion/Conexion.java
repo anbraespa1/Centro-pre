beb302181b1bed29cf127d81e097445ca2436392
anbraespa1 <feeling_105@hotmail.com>
2014-06-24 22:47:25 -0700

    Update Conexion.java

--- src/acadmia/conexion/Conexion.java
+++ src/acadmia/conexion/Conexion.java
@@ -11,5 +11,22 @@ package acadmia.conexion;
  * @author LADY ANN
  */
 public class Conexion {
+    package Conexion;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class Conexion {
+     public static Connection GetConexion() throws ClassNotFoundException, SQLException{
+    Connection conex=null;
+        Class.forName("com.mysql.jdbc.Driver");
+        String url="jdbc:mysql://localhost/Centro-pre";
+        String usuario="root";
+        String pass ="root";
+        conex =(Connection) DriverManager.getConnection(url,usuario,pass);
     
+    return conex;
+    }
 }
+
